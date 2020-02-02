9d5d0bbc9f4fe232bc5d4c4867cd06e3367ea0af
Dainius Laucys <laucysd@gmail.com>
2020-02-02 14:47:41 +0200

    arrow functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,69 +1,36 @@
 'use strict';
 
-console.log("App.js is running!");
-
-// JSX - Javascript XML (Javascript Syntax Extension)\
-
-var app = {
-  title: 'Indecision App',
-  subtitle: 'App subtitle',
-  options: ['One', 'Two']
+var add = function add(a, b) {
+    //console.log(arguments)
+    return a + b;
 };
 
-var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    app.title
-  ),
-  app.subtitle && React.createElement(
-    'p',
-    null,
-    app.subtitle
-  ),
-  React.createElement(
-    'p',
-    null,
-    app.options.length > 0 ? 'Here are your options' : 'No options'
-  )
-);
+console.log(add(55, 1, 1001));
 
 var user = {
-  name: 'Andrew',
-  age: 25,
-  location: 'Philadelphia'
+    name: 'Dainius',
+    cities: ['Vilnius', 'Telsiai', 'Kaunas'],
+    printPlacesLived: function printPlacesLived() {
+        var _this = this;
+
+        return this.cities.map(function (city) {
+            return _this.name + ' has lived in ' + city;
+        });
+    }
 };
 
-function getLocation(location) {
-  if (location) {
-    return React.createElement(
-      'p',
-      null,
-      'Location: ',
-      location
-    );
-  }
+console.log(user.printPlacesLived());
+
+var multiplier = {
+    numbers: [2, 4, 6],
+    multiplyBy: 2,
+    multiply: function multiply() {
+        var _this2 = this;
+
+        return this.numbers.map(function (number) {
+            return number * _this2.multiplyBy;
+        });
+    }
 };
 
-var templateTwo = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    user.name ? user.name : 'Anonymous'
-  ),
-  user.age && user.age >= 18 && React.createElement(
-    'p',
-    null,
-    'Age: ',
-    user.age
-  ),
-  getLocation(user.location)
-);
-
-var appRoot = document.getElementById("app");
-
-ReactDOM.render(template, appRoot);
+console.log(multiplier.multiply());
---
+++ src/playground/es6-arrow-function.js
@@ -0,0 +1,23 @@
+/* const square = function (x) {
+    return x * x;
+}; */
+
+/* const squareArrow = (x) => {
+    return x * x;
+}; */
+
+/* const squareArrow = (x) => x * x; */
+
+
+/* console.log(squareArrow(4)); */
+
+const fullName = 'Mike Smith';
+
+const getFirstName = (name) => {
+    return name.split(' ')[0];
+}
+console.log(getFirstName(fullName));
+
+
+const getFirstName = (name) => name.split(' ')[0];
+console.log(getFirstName('Mike Smith');
