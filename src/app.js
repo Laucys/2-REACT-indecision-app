f76006267b631667560155bc938c33f881a1972e
Dainius Laucys <laucysd@gmail.com>
2020-02-04 16:45:46 +0200

    creating components, nesting

--- src/app.js
+++ src/app.js
@@ -1,4 +1,68 @@
-class Header extends React.Component {
-  render
-
+class IndecisionApp extends React.Component{
+  render() {
+    return (
+      <div>
+      <Header />
+      <Action />
+      <Options />
+      <AddOption />
+      </div>
+    );
+  }
 }
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Indecision </h1>
+        <h2>Put your life in the hands of computer.</h2>
+      </div>
+    );
+  }
+}
+
+class Action extends React.Component {
+  render() {
+    return (
+      <div>
+        <button> What should I do?</button>
+      </div>
+    );
+  }
+}
+
+class Options extends React.Component {
+  render() {
+    return (
+      <div>
+        Options component here
+        <Option />
+      </div>
+    );
+  }
+}
+
+class Option extends React.Component {
+  render() {
+    return (
+      <div>
+        Option #1
+      </div>
+    );
+  }
+}
+
+class AddOption extends React.Component {
+  render() {
+    return (
+      <div>
+        AddOption component here
+      </div>
+    );
+  }
+}
+
+
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
